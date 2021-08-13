fdb2de6f21eb0c5e78b3e9cb659db6712632eae1
Vaibhav <simply.vaibhavgupta@gmail.com>
2021-08-13 10:23:15 +0530

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,154 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+
+class Graph<T> {
+
+    // We use Hashmap to store the edges in the graph
+    private Map<T, List<T> > map = new HashMap<>();
+
+    // This function adds a new vertex to the graph
+    public void addVertex(T s)
+    {
+        map.put(s, new LinkedList<T>());
+    }
+
+    // This function adds the edge
+    // between source to destination
+    public void addEdge(T source,
+                        T destination,
+                        boolean bidirectional)
+    {
+
+        if (!map.containsKey(source))
+            addVertex(source);
+
+        if (!map.containsKey(destination))
+            addVertex(destination);
+
+        map.get(source).add(destination);
+        if (bidirectional == true) {
+            map.get(destination).add(source);
+        }
+    }
+
+    // This function gives the count of vertices
+    public void getVertexCount()
+    {
+        System.out.println("The graph has "
+                + map.keySet().size()
+                + " vertex");
+    }
+
+    // This function gives the count of edges
+    public void getEdgesCount(boolean bidirection)
+    {
+        int count = 0;
+        for (T v : map.keySet()) {
+            count += map.get(v).size();
+        }
+        if (bidirection == true) {
+            count = count / 2;
+        }
+        System.out.println("The graph has "
+                + count
+                + " edges.");
+    }
+
+    // This function gives whether
+    // a vertex is present or not.
+    public void hasVertex(T s)
+    {
+        if (map.containsKey(s)) {
+            System.out.println("The graph contains "
+                    + s + " as a vertex.");
+        }
+        else {
+            System.out.println("The graph does not contain "
+                    + s + " as a vertex.");
+        }
+    }
+
+    // This function gives whether an edge is present or not.
+    public void hasEdge(T s, T d)
+    {
+        if (map.get(s).contains(d)) {
+            System.out.println("The graph has an edge between "
+                    + s + " and " + d + ".");
+        }
+        else {
+            System.out.println("The graph has no edge between "
+                    + s + " and " + d + ".");
+        }
+    }
+
+    // Prints the adjancency list of each vertex.
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        for (T v : map.keySet()) {
+            builder.append(v.toString() + " ");
+            for (T w : map.get(v)) {
+                builder.append(w.toString() + " ");
+            }
+            builder.append("\n");
+        }
+
+        return (builder.toString());
+    }
+}
+
+public class Main {
+    /**
+     * Iterate through each line of input.
+     */
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader in = new BufferedReader(reader);
+        String line;
+        int count=0;
+        int res[][] ;
+        // Object of graph is created.
+        Graph<Integer> g = new Graph<Integer>();
+
+        // edges are added.
+        // Since the graph is bidirectional,
+        // so boolean bidirectional is passed as true.
+
+        while ((line = in.readLine()) != null) {
+
+            String arr[]= line.split(" ");
+            // res[][] = new int [arr.size()][arr.size()];
+            for(int i=0;i<arr.length;i++){
+                if(Integer.parseInt(arr[i])==1){
+                    g.addEdge(count, i, true);
+                }
+            }
+            count ++;
+        }
+        //  System.out.println(arr[0]);
+   /*
+    for(int i=0;i<arr.length;i++){
+
+        if(Integer.parseInt(arr[i])==1){
+         // System.out.print(i + " ");
+          res[count][i] = i;
+        }
+             System.out.println(res[count][i]);
+      }
+
+    count ++;
+    }
+    */
+
+
+        // print the graph.
+        System.out.println(g.toString());
+    }
+}
+
